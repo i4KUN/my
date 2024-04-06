8cb6d5f0efe2e069677c3f36ee0631435e5af51e
i4KUN <40508682+i4KUN@users.noreply.github.com>
2024-04-06 04:44:49 +0300

    search4.js

---
+++ search4.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  var typingTimer;                // تأخير الكتابة قبل إرسال طلب البحث
+  var doneTypingInterval = 500;  // الوقت الذي ينتظره السكربت قبل إرسال طلب البحث بعد انتهاء الكتابة
+
+  // عند الكتابة في حقل البحث
+  $('#searchIn').on('input', function(){
+    clearTimeout(typingTimer);  // إذا كان هناك مؤقت تأخير سابق، قم بإلغائه
+    if ($('#searchIn').val()) {
+      typingTimer = setTimeout(doneTyping, doneTypingInterval); // إعادة ضبط المؤقت
+    }
+  });
+
+  // عند الانتهاء من الكتابة
+  function doneTyping() {
+    var searchTerm = $('#searchIn').val().toLowerCase();  // الكلمة المكتوبة في حقل البحث
+    $.ajax({
+      url: '/feeds/posts/default?q=' + searchTerm + '&alt=json',  // رابط طلب البحث
+      type: 'GET',
+      success: function(data){
+        // عرض النتائج هنا
+        // يمكنك استخدام دوال jQuery لتحديد مكان عرض النتائج وإضافتها إليه
+        console.log(data); // مثال: طباعة البيانات في وحدة التحكم للتحقق من عملية البحث
+      }
+    });
+  }
+});
